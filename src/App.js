a6201e49f9112aa913aa6c4ca853f920982486cd
zen-lenovo <zennomi.kun@gmail.com>
2023-11-25 12:50:38 +0700

    fix reponsive bug

--- src/App.js
+++ src/App.js
@@ -91,61 +91,63 @@ function App() {
   }, [position])
 
   return (
-    <div className="App w-[100vw] h-[100vh] bg-gray-50 dark:bg-gray-800">
-      <div className='container mx-auto pt-2 text-center'>
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white uppercase">Gương chiếu sếch</h1>
-        <a
-          target='_blank'
-          href="https://github.com/zennomi/Seg-Mirror"
-          className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900 mb-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-          >
-            <path
-              fill="currentColor"
-              d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
-            />
-          </svg>
-          Github
-        </a>
-        <div className='flex flex-col gap-y-2'>
-          <label for="images" className="block text-sm font-medium text-gray-900 dark:text-white">Chọn ảnh</label>
-          <select
-            id="images"
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            value={imageId}
-            onChange={(event) => {
-              setImageId(event.target.value)
-              bc.postMessage({ type: 'image_id', value: event.target.value })
+    <>
+      <div className="App w-[100vw] h-[100vh] bg-gray-50 dark:bg-gray-800">
+        <div className='container mx-auto text-center pt-2'>
+          <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900  dark:text-white uppercase mb-2">Gương chiếu sếch</h1>
+          <a
+            target='_blank'
+            href="https://github.com/zennomi/Seg-Mirror"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+              />
+            </svg>
+            Github
+          </a>
+          <div className='flex flex-col gap-y-2'>
+            <label for="images" className="block text-sm font-medium text-gray-900 dark:text-white">Chọn ảnh</label>
+            <select
+              id="images"
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              value={imageId}
+              onChange={(event) => {
+                setImageId(event.target.value)
+                bc.postMessage({ type: 'image_id', value: event.target.value })
+              }}
+            >
+              {
+                images.map((image, index) => <option value={index}>{image.title}</option>)
+              }
+            </select>
+            <button
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+              onClick={handleOpenNewWindow}>{
+                index === 0 ? "Cho 1 em nữ sinh cao trung ra đây" :
+                  index === 1 ? "Mở gương thần" : "Đóng gương thần"
+              }</button>
+          </div>
+        </div>
+        {index !== 0
+          &&
+          <Draggable
+            position={position}
+            onDrag={(event, data) => {
+              updateNewPosition({ x: data.x, y: data.y });
             }}
           >
-            {
-              images.map((image, index) => <option value={index}>{image.title}</option>)
-            }
-          </select>
-          <button
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-            onClick={handleOpenNewWindow}>{
-              index === 0 ? "Cho 1 em nữ sinh cao trung ra đây" :
-                index === 1 ? "Mở gương thần" : "Đóng gương thần"
-            }</button>
-        </div>
+            <img src={index === 2 ? image2Url : image1Url} draggable="false" />
+          </Draggable>
+        }
       </div>
-      {index !== 0
-        &&
-        <Draggable
-          position={position}
-          onDrag={(event, data) => {
-            updateNewPosition({ x: data.x, y: data.y });
-          }}
-        >
-          <img className='max-w-none' src={index === 2 ? image2Url : image1Url} draggable="false" />
-        </Draggable>
-      }
-    </div>
+    </>
   );
 }
 
